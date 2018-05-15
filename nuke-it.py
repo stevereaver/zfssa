a544e9b33c1268c6538f91a0cc104b1eac56cacc
Stephen Bancroft <stevereaver@gmail.com>
2018-05-15 11:21:54 +1000

    Update nuke-it.py

--- nuke-it.py
+++ nuke-it.py
@@ -14,18 +14,18 @@ HOST = 'https://10.2.0.195:215/'
 # Delete ALL LUNS
 headers = {"Content-Type": "application/json"}
 url = HOST+'api/storage/v1/luns'
-response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 nodeRef = json.loads(response.text)
 for lun in nodeRef['luns']:   
-    response = requests.delete(HOST+lun['href'], headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+    response = requests.delete(HOST+lun['href'], headers={'Authorization': 'Basic xx'},verify=False)
     print 'Deleted LUN '+HOST+lun['href'],response
 
 # Delete ALL Projects
 headers = {"Content-Type": "application/json"}
 url = HOST+'api/storage/v1/projects'
-response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 nodeRef = json.loads(response.text)
 for project in nodeRef['projects']: 
     if project['name'] != 'default':
-        response = requests.delete(HOST+project['href'], headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+        response = requests.delete(HOST+project['href'], headers={'Authorization': 'Basic xx'},verify=False)
         print 'Deleted project '+HOST+project['href'],response
