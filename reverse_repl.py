b417960c9dcf67e863f745e04b9898cf4e605827
Stephen Bancroft <stevereaver@gmail.com>
2018-05-15 11:20:50 +1000

    Update reverse_repl.py

--- reverse_repl.py
+++ reverse_repl.py
@@ -35,16 +35,16 @@ if answer != "yesiam":
 # Start by sending updates from source first
 # and wait for the update to finish
 url = 'https://'+HOST_DR+':215/api/storage/v1/replication/actions'
-response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 nodeRef = json.loads(response.text)
 for action in nodeRef['actions']:
     url = 'https://'+HOST_DR+':215/api/storage/v1/replication/actions'+action['id']+'/sendupdate'
-    response = requests.put(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+    response = requests.put(url, headers={'Authorization': 'Basic xx'},verify=False)
     print 'Sent replication update for '+action['replication_of']
     # If Replication action is scheduled wait for it to finish
     if action['continuous']==0:
         url = 'https://'+HOST_DR+':215/'+action['href']
-        response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+        response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
         nodeRef2 = json.loads(response.text)
         #print nodeRef2
         action2=nodeRef2['action']
@@ -52,14 +52,14 @@ for action in nodeRef['actions']:
             print 'Replication state is "'+action2['state']+'" waiting....'
             time.sleep(2)
             url = 'https://'+HOST_DR+':215/'+action['href']
-            response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+            response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
         print 'Replication state is '+action2['state']+' continuing'
     else:
         print 'Replication action is continuous'
 
 #Get source target ID
 #url = 'https://'+HOST_DR+':215/api/storage/v1/replication/sources'
-#response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+#response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 #nodeRef = json.loads(response.text)
 # We assume only one source, which should always be the case in our environment
 #SOURCE = nodeRef['sources'][0]['name']
@@ -67,7 +67,7 @@ for action in nodeRef['actions']:
 # Get a list of the replication packages
 url = 'https://'+HOST_DR+':215/api/storage/v1/replication/sources/' + SOURCE
 #print url
-response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 nodeRef = json.loads(response.text)
 #print nodeRef
 packages = nodeRef['source']['packages']
@@ -82,7 +82,7 @@ for package in packages:
         # Reverse it!
         url = 'https://'+HOST_DR+':215/api/storage/v1/replication/sources/' + SOURCE + '/packages/' + package['id'] + '/reverse'
         #print url, data
-        headers = {'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw==', 'Content-Type': 'application/json'}
+        headers = {'Authorization': 'Basic xx', 'Content-Type': 'application/json'}
         response = requests.put(url, headers=headers,data=json.dumps(data),verify=False)
         print "Reversing Replication for " + package['project'], response
         if response.status_code != 202:
@@ -92,7 +92,7 @@ for package in packages:
 
 # Enable ALL replication actions to continuous
 url = 'https://'+HOST_DR+':215/api/storage/v1/replication/actions'
-headers = {'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='}
+headers = {'Authorization': 'Basic xx'}
 response = requests.get(url, headers=headers,verify=False)
 nodeRef = json.loads(response.text)
 for action in nodeRef['actions']:
