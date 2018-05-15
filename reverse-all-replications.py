b8d4646f700cc0158bfd8fdfec433492ee2c4e8e
Stephen Bancroft <stevereaver@gmail.com>
2018-05-15 11:21:20 +1000

    Update reverse-all-replications.py

--- reverse-all-replications.py
+++ reverse-all-replications.py
@@ -13,14 +13,14 @@ HOST = 'https://10.2.0.191:215/'
 
 #Get source target ID
 url = HOST+'api/storage/v1/replication/sources'
-response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 nodeRef = json.loads(response.text)
 # We assume only one source, which should always be the case in our environment
 SOURCE = nodeRef['sources'][0]['name']
 
 # Get a list of the replication packages
 url = HOST+'api/storage/v1/replication/sources/' + SOURCE
-response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
 nodeRef = json.loads(response.text)
 packages = nodeRef['source']['packages']
 
@@ -30,13 +30,13 @@ for package in packages:
     
     #url = 'https://10.2.0.191:215' + package['href'] + '/reverse'
     #url = 'https://10.2.0.191:215' + package['href']
-    #response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+    #response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
     #nodeRef = json.loads(response.text)
 
     data= {'projname':package['project']}        
     url = HOST+'api/storage/v1/replication/sources/' + SOURCE + '/packages/' + package['id'] + '/reverse'
     #print url, data
-    headers = {'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw==', 'Content-Type': 'application/json'}
+    headers = {'Authorization': 'Basic xx', 'Content-Type': 'application/json'}
     response = requests.put(url, headers=headers,data=json.dumps(data),verify=False)
     print "Reversing Replication for " + package['project'], response
     if response.status_code != 202:
@@ -44,7 +44,7 @@ for package in packages:
         
 # Set ALL replication actions to continuous
 url = HOST+'api/storage/v1/replication/actions'
-headers = {'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='}
+headers = {'Authorization': 'Basic xx'}
 response = requests.get(url, headers=headers,verify=False)
 nodeRef = json.loads(response.text)
 for action in nodeRef['actions']:
