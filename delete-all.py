73b557bd133265e09a9a1b989faf0fca934697bf
Stephen Bancroft <stevereaver@gmail.com>
2018-05-15 11:24:29 +1000

    Update delete-all.py

--- delete-all.py
+++ delete-all.py
@@ -15,26 +15,26 @@ for HOST in HOSTS:
     # Delete ALL LUNS
     headers = {"Content-Type": "application/json"}
     url = HOST+'api/storage/v1/luns'
-    response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+    response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
     nodeRef = json.loads(response.text)
     for lun in nodeRef['luns']:   
-        response = requests.delete(HOST+lun['href'], headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+        response = requests.delete(HOST+lun['href'], headers={'Authorization': 'Basic xx'},verify=False)
         print 'Deleted LUN '+HOST+lun['href'],response
 
     # Delete ALL Projects
     headers = {"Content-Type": "application/json"}
     url = HOST+'api/storage/v1/projects'
-    response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+    response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
     nodeRef = json.loads(response.text)
     for project in nodeRef['projects']: 
         if project['name'] != 'default':
-            response = requests.delete(HOST+project['href'], headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+            response = requests.delete(HOST+project['href'], headers={'Authorization': 'Basic xx'},verify=False)
             print 'Deleted project '+HOST+project['href'],response
 
     # Delete ALL Replication Packages if they exist
     # Get source target ID
     url = HOST+'api/storage/v1/replication/sources'
-    response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+    response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
     nodeRef = json.loads(response.text)
     # We assume only one source, which should always be the case in our environment
     # Test if the string is empty, ie: do we really have a source
@@ -42,10 +42,10 @@ for HOST in HOSTS:
         SOURCE = nodeRef['sources'][0]['name']
         headers = {"Content-Type": "application/json"}
         url = HOST+'api/storage/v1/replication/sources/' + SOURCE
-        response = requests.get(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+        response = requests.get(url, headers={'Authorization': 'Basic xx'},verify=False)
         nodeRef = json.loads(response.text)
         for package in nodeRef['source']['packages']: 
             if package['project'] != 'default':
                 url = HOST+package['href']
-                response = requests.delete(url, headers={'Authorization': 'Basic cm9vdDpHMGRfNGM3Xw=='},verify=False)
+                response = requests.delete(url, headers={'Authorization': 'Basic xx'},verify=False)
                 print 'Deleted replication package '+HOST+package['project'],response
